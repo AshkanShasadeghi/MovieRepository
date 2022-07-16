1c94b018fc91d63373abf055d5ceb03b67be76de
Ashkan Shasadeghi <ashkansadeghi023.as@gmail.com>
2022-07-16 03:41:37 -0700

    Ahskan Shasadeghi

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,61 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Post from "./Components/Post";
-import Pagination from "./Components/Pagination";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [getPosts, setPosts] = useState([]);
-  const [getLoading, setLoading] = useState(false);
-  const [getCurrentPage, setCurrentPage] = useState(1);
-  const [getPostsPerPage, setPostsPerPage] = useState(10);
-
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-
-      const res = await axios.get("http://localhost:3000/Movie");
-      setPosts(res.data);
-    };
-
-    fetchPosts();
-  }, []);
-
-  const handleChangePerPage = (much) => {
-    if (much == 0) {
-      getPostsPerPage > 5 ? (setPostsPerPage(getPostsPerPage - 1)) : (setPostsPerPage(getPostsPerPage))
-    } else {
-      getPostsPerPage <= 14 ? (setPostsPerPage(getPostsPerPage + 1)) : (setPostsPerPage(getPostsPerPage))
-    }
-  }
-
-
-  const indexOfLastPost = getCurrentPage * getPostsPerPage;
-  const indexOfFirstPost = indexOfLastPost - getPostsPerPage;
-  const currentPosts = getPosts.slice(indexOfFirstPost, indexOfLastPost);
-
-  // Change page
-  const Paginate = (pageNumber) => setCurrentPage(pageNumber);
-
+function App() {
   return (
-    <>
-      <Post Posts={currentPosts} loading={getLoading} />
-      <Pagination
-        PostsPerPage={getPostsPerPage}
-        TotalPosts={getPosts.length}
-        Paginate={Paginate}
-      />
-      <section className="PerPage">
-        <button className="remove" onClick={() => handleChangePerPage(0)}>
-          <ion-icon name="remove-outline"></ion-icon>
-        </button>
-        <input type="number" value={getPostsPerPage} disabled />
-        <button className="add" onClick={() => handleChangePerPage(1)}>
-          <ion-icon name="add-outline"></ion-icon>
-        </button>
-      </section>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
